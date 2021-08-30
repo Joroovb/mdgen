543ec2857c82a4a791d3e34ab41da875db3d4b79
Joroovb <jorisvanbreugel@mailbox.org>
2021-08-30 12:02:31 +0200

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-echo "Hello world!"
+pandoc -s --template="templates/default.html" -f markdown-smart -c ../style/default.css "resume.md" -o "resume.html"
+wkhtmltopdf --enable-local-file-access "resume.html" "resume.pdf"
